bc04f118fa8a5607be1c4bdb6f0433a53c8a2b7f
Juan Sebastian Vargas <juan_vargas10@hotmail.com>
2016-02-10 16:54:45 -0500

    update

--- C/Runge/Juan-RungeKutta.c
+++ C/Runge/Juan-RungeKutta.c
@@ -17,5 +17,29 @@ double func_y(double t, double y);
 
 int main(void)
 {
+    double Runge2_y;
+    double Runge4_y;
+    double t;
     
+    double Tiempo= 2.0;
+    double paso=1E-3;
+    int numeroPasos=(int)(Tiempo/paso);
+    
+    
+    //iniciamos en tiempo 0
+    t=0.0;
+    //condiciones iniciales
+    Runge2_y=1.0;
+    Runge4_y=1.0;
+ 
+    
+}
+
+double RungePaso2(double paso,double t, double y, derivative dev)
+{
+    double k1,k2;
+    double paso_y;
+    k1=dev(t,y);
+    k2=dev(t+paso,y+paso*k1);
+    paso_y=0.5*(k1+k2)*paso;
 }
